b7ed5a8567779a377446e0a2366d6916be451120
rideau <93993119+adderallbaby@users.noreply.github.com>
2024-03-20 00:13:16 +0300

    Update lspconfig.lua

--- lua/nvchad/configs/lspconfig.lua
+++ lua/nvchad/configs/lspconfig.lua
@@ -90,11 +90,7 @@ M.defaults = function()
       },
     },
   }
-   require("lspconfig").pyink.setup{
-       on_attach = M.on_attach,
-       capabilities = M.capabilities,
-       on_init = M.on_init,
-   }
+
 end
 
 return M
