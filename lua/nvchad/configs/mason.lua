292e0b137c37b0357de8cf21481de9a7d425412c
rideau <93993119+adderallbaby@users.noreply.github.com>
2024-03-19 22:40:04 +0300

    Update mason.lua

--- lua/nvchad/configs/mason.lua
+++ lua/nvchad/configs/mason.lua
@@ -1,5 +1,5 @@
 local options = {
-  ensure_installed = { "lua-language-server",  'stylua' , 'python-lsp-server', 'clang-format', 'pyink'}, -- not an option from mason.nvim
+  ensure_installed = { "lua-language-server",  'stylua' }, -- not an option from mason.nvim
 
   PATH = "skip",
 
