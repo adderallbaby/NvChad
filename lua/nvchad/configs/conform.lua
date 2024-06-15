0a2879ce4e5d715384d94883d19fb4f82e16420c
rideau <93993119+adderallbaby@users.noreply.github.com>
2024-06-16 00:51:28 +0300

    Create conform.lua

---
+++ lua/nvchad/configs/conform.lua
@@ -0,0 +1,22 @@
+local options = {
+  formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "autopep8", "ruff"},
+        bash = {"shfmt"},
+        go = {"ast-grep"},
+        c = {"ast-grep"},
+        java = {"ast-grep"},
+        kotlin = {"ast-grep"},
+        javascript = {"ast-grep"},
+        cpp = {"ast-grep"},
+        html = {"ast-grep"},
+  },
+
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 500,
+  --   lsp_fallback = true,
+  -- },
+}
+
+require("conform").setup(options)
